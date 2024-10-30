ed3e5f70b5b3d3bea86f515462de5e2fd0f6d151
Tadas Baltrunas <tadas@baltrunas.lt>
2024-10-30 23:34:43 +0200

    implementing text input encoding, decoding

--- TextPage.Designer.cs
+++ TextPage.Designer.cs
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.TextBoxTextInput = new System.Windows.Forms.TextBox();
-            this.ButtonSendTextInput = new System.Windows.Forms.Button();
+            this.TextBoxInput = new System.Windows.Forms.TextBox();
+            this.ButtonSend = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.TextBoxWithEncoding = new System.Windows.Forms.TextBox();
             this.TextBoxWithoutEncoding = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextBoxProbability = new System.Windows.Forms.TextBox();
+            this.ButtonHome = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -47,29 +50,29 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter Text:";
             // 
-            // TextBoxTextInput
+            // TextBoxInput
             // 
-            this.TextBoxTextInput.Location = new System.Drawing.Point(28, 65);
-            this.TextBoxTextInput.Multiline = true;
-            this.TextBoxTextInput.Name = "TextBoxTextInput";
-            this.TextBoxTextInput.Size = new System.Drawing.Size(800, 125);
-            this.TextBoxTextInput.TabIndex = 1;
+            this.TextBoxInput.Location = new System.Drawing.Point(28, 65);
+            this.TextBoxInput.Multiline = true;
+            this.TextBoxInput.Name = "TextBoxInput";
+            this.TextBoxInput.Size = new System.Drawing.Size(600, 100);
+            this.TextBoxInput.TabIndex = 1;
             // 
-            // ButtonSendTextInput
+            // ButtonSend
             // 
-            this.ButtonSendTextInput.Location = new System.Drawing.Point(28, 221);
-            this.ButtonSendTextInput.Name = "ButtonSendTextInput";
-            this.ButtonSendTextInput.Size = new System.Drawing.Size(143, 43);
-            this.ButtonSendTextInput.TabIndex = 2;
-            this.ButtonSendTextInput.Text = "Send";
-            this.ButtonSendTextInput.UseVisualStyleBackColor = true;
-            this.ButtonSendTextInput.Click += new System.EventHandler(this.ButtonSendTextInput_Click);
+            this.ButtonSend.Location = new System.Drawing.Point(28, 254);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(143, 43);
+            this.ButtonSend.TabIndex = 2;
+            this.ButtonSend.Text = "Send";
+            this.ButtonSend.UseVisualStyleBackColor = true;
+            this.ButtonSend.Click += new System.EventHandler(this.ButtonSend_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 323);
+            this.label2.Location = new System.Drawing.Point(28, 363);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(315, 25);
             this.label2.TabIndex = 3;
@@ -79,7 +82,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 519);
+            this.label3.Location = new System.Drawing.Point(28, 525);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(348, 25);
             this.label3.TabIndex = 4;
@@ -87,35 +90,66 @@
             // 
             // TextBoxWithEncoding
             // 
-            this.TextBoxWithEncoding.Location = new System.Drawing.Point(28, 362);
+            this.TextBoxWithEncoding.Location = new System.Drawing.Point(28, 402);
             this.TextBoxWithEncoding.Multiline = true;
             this.TextBoxWithEncoding.Name = "TextBoxWithEncoding";
             this.TextBoxWithEncoding.ReadOnly = true;
-            this.TextBoxWithEncoding.Size = new System.Drawing.Size(800, 125);
+            this.TextBoxWithEncoding.Size = new System.Drawing.Size(600, 100);
             this.TextBoxWithEncoding.TabIndex = 7;
             // 
             // TextBoxWithoutEncoding
             // 
-            this.TextBoxWithoutEncoding.Location = new System.Drawing.Point(28, 556);
+            this.TextBoxWithoutEncoding.Location = new System.Drawing.Point(28, 562);
             this.TextBoxWithoutEncoding.Multiline = true;
             this.TextBoxWithoutEncoding.Name = "TextBoxWithoutEncoding";
             this.TextBoxWithoutEncoding.ReadOnly = true;
-            this.TextBoxWithoutEncoding.Size = new System.Drawing.Size(800, 125);
+            this.TextBoxWithoutEncoding.Size = new System.Drawing.Size(600, 100);
             this.TextBoxWithoutEncoding.TabIndex = 8;
             // 
-            // PageHandlingTextInput
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(263, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Enter error probablity p:";
+            // 
+            // TextBoxProbability
+            // 
+            this.TextBoxProbability.Location = new System.Drawing.Point(312, 197);
+            this.TextBoxProbability.Multiline = true;
+            this.TextBoxProbability.Name = "TextBoxProbability";
+            this.TextBoxProbability.Size = new System.Drawing.Size(148, 31);
+            this.TextBoxProbability.TabIndex = 10;
+            // 
+            // ButtonHome
+            // 
+            this.ButtonHome.Location = new System.Drawing.Point(667, 702);
+            this.ButtonHome.Name = "ButtonHome";
+            this.ButtonHome.Size = new System.Drawing.Size(143, 43);
+            this.ButtonHome.TabIndex = 11;
+            this.ButtonHome.Text = "Home";
+            this.ButtonHome.UseVisualStyleBackColor = true;
+            this.ButtonHome.Click += new System.EventHandler(this.ButtonHome_Click);
+            // 
+            // TextPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 704);
+            this.ClientSize = new System.Drawing.Size(822, 757);
+            this.Controls.Add(this.ButtonHome);
+            this.Controls.Add(this.TextBoxProbability);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.TextBoxWithoutEncoding);
             this.Controls.Add(this.TextBoxWithEncoding);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.ButtonSendTextInput);
-            this.Controls.Add(this.TextBoxTextInput);
+            this.Controls.Add(this.ButtonSend);
+            this.Controls.Add(this.TextBoxInput);
             this.Controls.Add(this.label1);
-            this.Name = "PageHandlingTextInput";
+            this.Name = "TextPage";
             this.Text = "Gollay Code Simulator";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -125,11 +159,14 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox TextBoxTextInput;
-        private System.Windows.Forms.Button ButtonSendTextInput;
+        private System.Windows.Forms.TextBox TextBoxInput;
+        private System.Windows.Forms.Button ButtonSend;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox TextBoxWithEncoding;
         private System.Windows.Forms.TextBox TextBoxWithoutEncoding;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TextBoxProbability;
+        private System.Windows.Forms.Button ButtonHome;
     }
 }
--- TextPage.cs
+++ TextPage.cs
@@ -18,47 +18,82 @@ namespace Golay_Code
             this.FormClosed += new FormClosedEventHandler(OnFormClosed);
         }
 
-        private void OnFormClosed(object sender, FormClosedEventArgs e)
+        private void ButtonSend_Click(object sender, EventArgs e)
         {
-            Application.Exit();
-        }
+            string input = TextBoxInput.Text;
 
-        private void ButtonSendTextInput_Click(object sender, EventArgs e)
-        {
-            string input = TextBoxTextInput.Text;
-
-            // Convert the text to bytes
+            // Step 1: Convert text to bits
             byte[] bytes = Encoding.UTF8.GetBytes(input);
-
-            // Calculate the length of the bit array (8 bits per byte)
             int[] bitArray = new int[bytes.Length * 8];
-
-            // Convert each byte to bits and store in the int[] array
             int bitIndex = 0;
             foreach (byte b in bytes)
             {
                 for (int i = 7; i >= 0; i--)
                 {
-                    // Extract each bit (0 or 1) and store in the bit array
                     bitArray[bitIndex++] = (b >> i) & 1;
                 }
             }
-            
-            int neddedBits = 12 - bitArray.Length % 12;
 
-            int[] textConvertedToBits = new int[bitArray.Length + neddedBits];
-            Array.Copy(bitArray, textConvertedToBits, bitArray.Length);
+            // Step 2: Pad the bit array to make its length a multiple of 12
+            int neededBits = 12 - (bitArray.Length % 12);
+            int[] paddedBitArray = new int[bitArray.Length + neededBits];
+            Array.Copy(bitArray, paddedBitArray, bitArray.Length);
 
+            // Step 3: Split padded bit array into 12-bit vectors
+            List<int[]> vectors = SplitBitArray(paddedBitArray);
 
-            TextBoxWithEncoding.Text = string.Join(" ", textConvertedToBits);
+            // Step 4: Encode and send each vector through the channel
 
-            List<int[]> vectors = SplitBitArray(textConvertedToBits);
+            double errorProbability = Channel.ParseProbability(TextBoxProbability.Text);
 
-            // Display the vectors
+            List<int> encodedAndDecodedBits = new List<int>();
+            List<int> noisyUnencodedBits = new List<int>();
+
             foreach (var vector in vectors)
             {
-                //Page1.EncodeVector(vector, Page1.matrix);
+                // Encode, send through channel, and decode
+                int[] encodedVector = Encoder.Encode(vector, Matrices.GetMatrixG());
+                int[] noisyEncodedVector = Channel.SimulateNoisyChannel(encodedVector, errorProbability);
+                int[] decodedVector = Decoder.Decode(noisyEncodedVector, Matrices.GetMatrixH(), Matrices.GetMatrixB());
+                encodedAndDecodedBits.AddRange(decodedVector);
+
+                // Send original (unencoded) vector through channel
+                int[] noisyUnencodedVector = Channel.SimulateNoisyChannel(vector, errorProbability);
+                noisyUnencodedBits.AddRange(noisyUnencodedVector);
             }
+
+            // Step 5: Convert the resulting noisy unencoded bit sequence back to text
+            int[] noisyUnencodedBitArray = noisyUnencodedBits.ToArray();
+            string decodedTextWithoutEncoding = ConvertBitsToText(noisyUnencodedBitArray, neededBits);
+
+            // Step 6: Convert the corrected bit sequence back to text
+            int[] correctedBitArray = encodedAndDecodedBits.ToArray();
+            string decodedTextWithEncoding = ConvertBitsToText(correctedBitArray, neededBits);
+
+            // Step 7: Display results
+            TextBoxWithEncoding.Text = decodedTextWithEncoding;      
+            TextBoxWithoutEncoding.Text = decodedTextWithoutEncoding;
+        }
+
+
+        private string ConvertBitsToText(int[] bitArray, int paddingBits)
+        {
+            // Remove padding bits
+            int validLength = bitArray.Length - paddingBits;
+            List<byte> bytes = new List<byte>();
+
+            // Convert every 8 bits back into a byte
+            for (int i = 0; i < validLength; i += 8)
+            {
+                int byteValue = 0;
+                for (int j = 0; j < 8; j++)
+                {
+                    byteValue = (byteValue << 1) | bitArray[i + j];
+                }
+                bytes.Add((byte)byteValue);
+            }
+
+            return Encoding.UTF8.GetString(bytes.ToArray());
         }
 
         public static List<int[]> SplitBitArray(int[] bitArray)
@@ -66,24 +101,27 @@ namespace Golay_Code
             int vectorLength = 12;
             List<int[]> vectors = new List<int[]>();
 
-            // Calculate the number of complete vectors of length vectorLength
             int totalVectors = bitArray.Length / vectorLength;
-
             for (int i = 0; i < totalVectors; i++)
             {
-                // Create a new vector of length vectorLength
                 int[] vector = new int[vectorLength];
-
-                // Copy the relevant segment from the bitArray to the vector
                 Array.Copy(bitArray, i * vectorLength, vector, 0, vectorLength);
-
-                // Add the vector to the list of vectors
                 vectors.Add(vector);
             }
 
             return vectors;
         }
 
+        private void ButtonHome_Click(object sender, EventArgs e)
+        {
+            HomePage welcomePage = new HomePage();
+            welcomePage.Show();
+            this.Hide();
+        }
 
+        private void OnFormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
